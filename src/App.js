04b7692acb6fc6245c3ca13ab1a2776e9a2b60c0
Son Juwan <113279387+vvan2@users.noreply.github.com>
2024-10-05 14:47:08 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          빈프로젝트 내가 이렇게 비마
+          내가 이렇게 비마 제발 되라 제발 되라 제발 되라 제발 되라 제발 되라 제발 되라 제발되라 제발 되라 제발 되라
         </p>
         <a
           className="App-link"
