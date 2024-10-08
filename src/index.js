ef2199d781b735ec269612861daf428e05b99d05
Son Juwan <113279387+vvan2@users.noreply.github.com>
2024-10-08 14:17:58 +0900

    Feat: 메인화면 구현

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import MyPage from './page/mypage';
+import MainPage from './mainpage/main_page';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
 
---
+++ src/mainpage/main_page.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './vars.css';
+import './style.css';
+
+const main_page = () => {
+    return (
+        <div className="main-page">
+            <div className="div2"></div>
+            <div className="div3">
+                <div className="rectangle-2"></div>
+                <div className="div4">카카오톡</div>
+            </div>
+            <div className="div5">
+                <div className="rectangle-22"></div>
+                <div className="div6">문자</div>
+            </div>
+            <div className="div7"></div>
+            <div className="div8">문자</div>
+            <div className="div9">미리보기</div>
+            <div className="div10">발송하기</div>
+            <div className="div11">메시지 입력</div>
+            <div className="div12"></div>
+            <div className="div13"></div>
+            <div className="or-gif">이미지 or GIF 추가</div>
+            <div className="div14"></div>
+            <div className="div15">내용을 입력해주세요.</div>
+            <div className="div16"></div>
+            <div className="div17">제목을 입력해주세요..</div>
+            <img className="x" src="x0.svg" alt="x icon" />
+            <div className="div18"></div>
+            <div className="div19"></div>
+            <div className="div20"></div>
+            <img className="div21" src="div21.svg" alt="div21" />
+            <div className="x2"></div>
+            <div className="div22"></div>
+            <img className="x3" src="x2.svg" alt="x3 icon" />
+            <img className="div23" src="div23.svg" alt="div23" />
+            <div className="rectangle-4"></div>
+            <div className="div24"></div>
+            <div className="div25"></div>
+            <div className="div26">받는 사람</div>
+            <div className="div27">수신번호 입력</div>
+            <div className="ai">AI 자동 생성</div>
+            <div className="rectangle-5"></div>
+            <div className="div28">번호 추가</div>
+            <div className="rectangle-6"></div>
+            <div className="rectangle-7"></div>
+            <div className="div29">발송하기</div>
+            <div className="header">
+                <div className="blue-print">BluePrint</div>
+                <img className="user-profile-01" src="user-profile-010.svg" alt="user profile" />
+                <div className="div30">회원이름</div>
+                <div className="div31">로그아웃</div>
+                <div className="div32">마이페이지</div>
+            </div>
+        </div>
+    );
+}
+
+export default main_page;
